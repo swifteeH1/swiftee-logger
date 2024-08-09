97127ebf3b50b363bd7524205f3006239c69db3a
swifteeH1 <swifteee@wearehackerone.com>
2024-08-09 11:21:32 +0300

    add wiki redirect page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/wiki',
+        destination: 'https://wikipedia.org',
+        permanent: false,
+      },
+    ]
+  }
 };
 
 module.exports = nextConfig;
