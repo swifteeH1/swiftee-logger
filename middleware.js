1af00a39955eb4eb86ff109ab5b957a2bea8c9cf
swifteeH1 <swifteee@wearehackerone.com>
2024-09-02 23:27:24 +0300

    added metadata get redirects

--- middleware.js
+++ middleware.js
@@ -1,9 +1,19 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-    return NextResponse.redirect("https://wikipedia.org", { status: 303 })
+    if (request.nextUrl.pathname.startsWith('/klarna-redirect')) {
+        return NextResponse.redirect("http://wikipedia.org", { status: 303 })
+    };
+
+    if (request.nextUrl.pathname.startsWith('/aws-metadata')) {
+        return NextResponse.redirect("http://169.254.169.254", { status: 302 })
+    };
+
+    if (request.nextUrl.pathname.startsWith('/aws-metadata-ssl')) {
+        return NextResponse.redirect("https://169.254.169.254", { status: 302 })
+    };
 };
 
 export const config = {
-    matcher: '/klarna'
+    matcher: ['/klarna-redirect', '/aws-metadata', '/aws-metadata-ssl']
 };
