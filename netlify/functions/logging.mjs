3502723d524c9b78f1d0289589ee43ac9ab84be1
swifteeH1 <swifteee@wearehackerone.com>
2025-07-22 15:45:05 +0300

    added headers to logging function

--- netlify/functions/logging.mjs
+++ netlify/functions/logging.mjs
@@ -1,8 +1,16 @@
 export default async (req) => {
     console.log(req);
-    return new Response(`Hello There!`);
+    return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type",
+        },
+        body: JSON.stringify({ message: "Hello from Netlify Function" }),
+      };
 };
 
 export const config = {
-    path: "/log"
+    path: "/"
 };
